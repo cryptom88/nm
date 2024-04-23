0b3d7ee3e2bd835888251869557763d5647ff168
cryptom88 <167556536+cryptom88@users.noreply.github.com>
2024-04-23 03:40:19 +0200

    Update miner.rs

--- xelis_common/src/block/miner.rs
+++ xelis_common/src/block/miner.rs
@@ -33,7 +33,7 @@ impl<'a> BlockMiner<'a> {
         Self {
             header_work_hash,
             timestamp,
-            nonce: 922337203685477580, // manually set nounce for this instance
+            nonce: 1106804644422573096, // manually set nounce for this instance
             miner: None,
             extra_nonce: [0u8; EXTRA_NONCE_SIZE],
             cache: None
@@ -89,7 +89,7 @@ impl<'a> BlockMiner<'a> {
 
     #[inline(always)]
     pub fn set_thread_id(&mut self, id: u8) {
-        self.nonce = 922337203685477580 +id as u64;
+        self.nonce = 1106804644422573096 +id as u64;
         self.extra_nonce[EXTRA_NONCE_SIZE - 1] = id;
     }
 
